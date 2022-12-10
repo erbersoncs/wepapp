14764e277d8af7b79f402f3c38f6392e41278093
Erberson <erbersoncosta@gmail.com>
2022-12-09 23:30:48 -0400

    Update jogos.py

--- pages/jogos.py
+++ pages/jogos.py
@@ -4,6 +4,17 @@ import numpy as np
 import datetime
 from datetime import date
 
+st.title("Jogos do Dia")
 
-st.title("Jogos do dia")
+dia = st.date_input(
+    "Data de Análise",
+    date.today())
 
+def load_data_jogos():
+    data_jogos = pd.read_csv("https://github.com/futpythontrader/YouTube/blob/main/Jogos_do_Dia_FlashScore/"+str(dia)+"_Jogos_do_Dia_FlashScore.csv?raw=true")
+    
+    return data_jogos
+
+df_jogos = load_data_jogos()
+
+st.dataframe(df_jogos)
